3dcc4c7df7a3035205cb72290ddd601278833313
alvaroizv <abecrob2410@ieszaidinvergeles.org>
2026-01-22 14:40:33 +0100

    Creamos fichero js sin cluster

--- express.sh
+++ express.sh
@@ -6,4 +6,16 @@ apt-get update
 
 apt-get -y upgrade
 
-apt-get -y install nodejs
+# Actualizar la versión 20 de node.js
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+
+apt-get -y install nodejs 
+
+mkdir -p proyecto_cluster
+cd proyecto_cluster
+
+npm init -y
+
+npm install express
+
+cp /vagrant/app_sinCluster.js .
